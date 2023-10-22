1a9246b480ec5057e72a8c9894e9d89eb310fb0a
migueltovar <loganitsolutions@outlook.com>
2023-10-22 03:13:03 -0500

    worker finished

--- flaskr/tasks/tasks.py
+++ flaskr/tasks/tasks.py
@@ -25,18 +25,64 @@ TASK_POSTED, = config.TASK_POSTED_TOPICS
 @celery_app.task(name=TASK_POSTED)
 def convert_video(task_json):
     print(task_json)
+    idvideo=task_json['id']
     input_file=config.PATH_STORAGE+'input/'+task_json['id']+task_json['fileName']
     extension = os.path.splitext(input_file)[1]
-    print(task_json['newFormat'])
-    print(extension.upper())
-    if(task_json['newFormat']=='MP4' and extension.upper().replace('.','')=='AVI'):
-        converter.avitomp4(input_file,task_json['fileName'])
+    destination=task_json['newFormat']
+    origin=extension.upper().replace('.','')
+    outputFileName=task_json['fileName']
+
+    #MP4	WEBM	AVI	MPEG	WMV
+
+    status='processed'
+
+    if(origin=='MP4' and destination=='WEBM'):
+        converter.mp4towebm(idvideo,input_file,outputFileName)
+    elif(origin=='MP4' and destination=='AVI'):
+        converter.mp4toavi(idvideo,input_file,outputFileName)
+    elif(origin=='MP4' and destination=='MPEG'):
+        converter.mp4tompeg(idvideo,input_file,outputFileName)
+    elif(origin=='MP4' and destination=='WMV'):
+        converter.mp4towmv(idvideo,input_file,outputFileName)
+    elif(origin=='WEBM' and destination=='MP4'):
+        converter.webmtomp4(idvideo,input_file,outputFileName)
+    elif(origin=='WEBM' and destination=='AVI'):
+        converter.webmtoavi(idvideo,input_file,outputFileName)
+    elif(origin=='WEBM' and destination=='MPEG'):
+        converter.webmtompeg(idvideo,input_file,outputFileName)
+    elif(origin=='WEBM' and destination=='WMV'):
+        converter.webmtowmv(idvideo,input_file,outputFileName)
+    elif(origin=='AVI' and destination=='MP4'):
+        converter.avitomp4(idvideo,input_file,outputFileName)
+    elif(origin=='AVI' and destination=='WEBM'):
+        converter.avitowebm(idvideo,input_file,outputFileName)
+    elif(origin=='AVI' and destination=='MPEG'):
+        converter.avitompeg(idvideo,input_file,outputFileName)
+    elif(origin=='AVI' and destination=='WMV'):
+        converter.avitowmv(idvideo,input_file,outputFileName)
+    elif(origin=='MPEG' and destination=='MP4'):
+        converter.mpegtomp4(idvideo,input_file,outputFileName)
+    elif(origin=='MPEG' and destination=='WEBM'):
+        converter.mpegtowebm(idvideo,input_file,outputFileName)
+    elif(origin=='MPEG' and destination=='AVI'):
+        converter.mpegtoavi(idvideo,input_file,outputFileName)
+    elif(origin=='MPEG' and destination=='WMV'):
+        converter.mpegtowmv(idvideo,input_file,outputFileName)
+    elif(origin=='WMV' and destination=='MP4'):
+        converter.wmvmtomp4(idvideo,input_file,outputFileName)
+    elif(origin=='WMV' and destination=='WEBM'):
+        converter.wmvtowebm(idvideo,input_file,outputFileName)
+    elif(origin=='WMV' and destination=='AVI'):
+        converter.wmvmtoavi(idvideo,input_file,outputFileName)
+    elif(origin=='WMV' and destination=='MPEG'):
+        converter.wmvtompeg(idvideo,input_file,outputFileName)
     else:
-        print('no supported')
+        print('Conversion no supported')
+        status='Conversion no supported'
 
     #changing status in database
     conversion=Conversion.query.filter_by(id=int(task_json['id'])).one()
-    conversion.status='processed'
+    conversion.status=status
     db.session.commit()
 
     print('video converted success')
